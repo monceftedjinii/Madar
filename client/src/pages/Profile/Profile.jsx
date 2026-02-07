f094734f7c9fcbbe8683ec6965f4183d200e66d8
doudaoud <boudaoudreda1@gmail.com>
2026-02-07 22:58:47 +0100

    feat: Add initial Profile page component with user data fetching.

--- client/src/pages/Profile/Profile.jsx
+++ client/src/pages/Profile/Profile.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
-
+﻿import { useEffect } from "react";
+import axios from "axios";
 export default function Profile() {
+  let access = localStorage.getItem("access_token");
+  const fetchProfile = async () => {
+    const me = await axios.get("/api/whoami/", {
+      headers: { Authorization: `Bearer ${access}` },
+    });
+    console.log(me.data);
+  };
+  useEffect(() => {
+    fetchProfile();
+  }, []);
+
   return (
-    <div>Profile page pour user </div>
+    <>
+        <h1>Profile Page</h1>       
+    </>
   )
 }
