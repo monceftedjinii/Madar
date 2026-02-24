c9cba1492b52dc615dbe81f6b3637b19a08b67f5
doudaoud <boudaoudreda1@gmail.com>
2026-02-24 17:35:32 +0100

    feat: add profile page layout and styling, include Navbar component

--- client/src/pages/Profile/Profile.jsx
+++ client/src/pages/Profile/Profile.jsx
@@ -1,22 +1,38 @@
-﻿import { useEffect } from "react";
-import axios from "axios";
+﻿// import { useEffect } from "react";
+// import axios from "axios";
+import "../../styles/profile.css";
 import Navbar from "../../components/Navbar";
 export default function Profile() {
-  let access = localStorage.getItem("access_token");
-  const fetchProfile = async () => {
-    const me = await axios.get("/api/whoami/", {
-      headers: { Authorization: `Bearer ${access}` },
-    });
-    console.log(me.data);
-  };
-  useEffect(() => {
-    fetchProfile();
-  }, []);
+  // let access = localStorage.getItem("access_token");
+  // const fetchProfile = async () => {
+  //   const me = await axios.get("/api/whoami/", {
+  //     headers: { Authorization: `Bearer ${access}` },
+  //   });
+  //   console.log(me.data);
+  // };
+  // useEffect(() => {
+  //   fetchProfile();
+  // }, []);
 
   return (
     <>
       <div className="profile-page">
-        <Navbar />
+        <div className="navbar-profile-page">
+          <Navbar />
+        </div>
+        <div className="profile-content">
+          <div className="profile-naaav">
+            <div className="yasar">
+              <h3>Mon Profil</h3>
+              <p>Informations personnelles • poste • photo</p>
+            </div>
+            <div className="yamin">
+              <button className="mode">mode sombre</button>
+              <button className="deconnecter">déconnecter</button>
+              <button className="modifier">modifier</button>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
