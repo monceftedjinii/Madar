0ec073e1e9db959777e74448b328dab44ea53cef
doudaoud <boudaoudreda1@gmail.com>
2026-02-28 02:05:00 +0100

    feat: Create error page with 404 message and navigation links

--- client/src/pages/Error.jsx
+++ client/src/pages/Error.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import "../styles/error.css";
+import logo from "../Assets/Logo_madar_holding.png";
 
 export default function Error() {
   return (
-    <div>Error</div>
-  )
+    <main className="error-page">
+      <div className="error-card">
+        <img src={logo} alt="Madar Holding" className="error-logo" />
+        <span className="error-code">404</span>
+        <h1>Page introuvable</h1>
+        <p>
+          Le lien demande n&apos;existe pas ou a ete deplace. Vous pouvez revenir
+          a l&apos;accueil pour continuer votre navigation.
+        </p>
+        <div className="error-actions">
+          <Link className="error-button primary" to="/home">
+            Retour a l&apos;accueil
+          </Link>
+          <Link className="error-button ghost" to="/login">
+            Aller a la connexion
+          </Link>
+        </div>
+      </div>
+    </main>
+  );
 }
