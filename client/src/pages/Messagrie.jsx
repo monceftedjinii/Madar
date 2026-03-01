a420b7fae5c75db54caeea25aee748feee2d1ecd
doudaoud <boudaoudreda1@gmail.com>
2026-03-01 11:50:14 +0100

    feat: Update Messagrie page layout and content for improved user experience

--- client/src/pages/Messagrie.jsx
+++ client/src/pages/Messagrie.jsx
@@ -1,35 +1,58 @@
 import React from "react";
-
+import Navbar from "../components/Navbar";
+import { useState } from "react";
 export default function Messagrie() {
+  const [dark, setDark] = useState(false);
+  const [isNavOpen, setIsNavOpen] = useState(true);
   return (
     <>
-      <div>
-         <div className="profile-naaav">
+      <div
+        className={`profile-page${dark ? " dark" : ""} ${
+          isNavOpen ? "nav-open" : "nav-closed"
+        }`}
+      >
+        <div className={`navbar-profile-page ${isNavOpen ? "open" : "closed"}`}>
+          <Navbar />
+        </div>
+        {isNavOpen && (
+          <div
+            className="profile-overlay"
+            onClick={() => setIsNavOpen(false)}
+            aria-hidden="true"
+          />
+        )}
+        <div className="profile-content">
+          <div
+            style={{
+              borderBottom: "1px solid rgba(0, 0, 0, 0.1)",
+            }}
+          >
+            <div className="profile-naaav">
               <div className="yasar">
-                <h3 className="monprofile">Mon Profil</h3>
-                <p className="morinfo">
-                  Informations personnelles • poste • photo
-                </p>
+                <h3 className="monprofile">Mail</h3>
+                <p className="morinfo">Plateforme interne • style boîte mail</p>
               </div>
               <div className="yamin">
                 <button
                   className="nav-toggle"
-                //   onClick={() => setIsNavOpen((prev) => !prev)}
+                  onClick={() => setIsNavOpen((prev) => !prev)}
                   type="button"
                 >
-                  {/* {isNavOpen ? "Masquer menu" : "Afficher menu"} */}
+                  {isNavOpen ? "Masquer menu" : "Afficher menu"}
                 </button>
-                <button className="mode" >
-                    {/* onClick={() => setDark(!dark)} */}
-                  {/* {dark ? " mode clair" : " mode sombre"} */} sdfsdfsdf
+                <button className="mode" onClick={() => setDark(!dark)}>
+                  {dark ? " mode clair" : " mode sombre"}
                 </button>
-                <button className="btn-logout">déconnecter</button>
-                <button className="modifier" type="button" >
-                    {/* onClick={openEditModal} */}
-                  modifier
+
+                <button className="modifier" type="button"
+                
+                >
+                  nouveau message
                 </button>
               </div>
             </div>
+          </div>
+        </div>
       </div>
     </>
   );
